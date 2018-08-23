5f0ffe275ee7d24c78dd0eeafc4c8948048504bb
Jaineshkumar Patel <jenu.16@icloud.com>
2018-08-23 13:40:48 -0500

    Cat Clicker Premium refactored with separation of concerns

--- app.js
+++ app.js
@@ -1,36 +1,113 @@
+'use strict';
 
-var cats = $(".cat");
-var buttons = $("button");
+$(() => {
+  let model = {
+    // define cat names
+    catList: [
+      'bubbly',
+      'chewie',
+      'kathy',
+      'lyza',
+      'xuca'
+    ],
+    catClicksCount: {},
+    // initalizer for clicks on each cat
+    init: () => {
+      for (const cat of model.catList) {
+        model.catClicksCount[cat] = 0;
+      }
+    }
+  };
 
-function hideAllCats(){
-	for (var i=0; i<cats.length; i++){
-		$(cats[i]).hide();
-	}
-}
+  let listView = {
+    // select DOM elements to work with
+    menu: $('.cat-menu'),
+    init: () => {
+      listView.render();
+      listView.handleClicks();
+    },
+    // handle clicks on cat names
+    handleClicks: () => {
+      listView.menu.click((event) => {
+        const TARGET = event.target.dataset.cat;
+        if (TARGET) {
+          // render cat in display area upon cat name being clicked
+          octopus.displayCat = TARGET;
+          octopus.renderCat();
+        } else {
+          return;
+        }
+      });
+    },
+    // render cat list
+    render: () => {
+      // obtain cat list from octopus
+      const catList = octopus.getCatList();
+      for (const cat of catList) {
+        listView.menu.append(`<li class=\"cat-name\" data-cat=\"${cat}\">${octopus.capitlizeCatName(cat)}</li>`);
+      }
+    }
+  };
 
-function bindButtonToCat(idNumber){
-	$("#button"+idNumber).click(function(){
-		hideAllCats();
-		$("#cat"+idNumber).show();
-	})
-}
+  let catView = {
+    catContainer: $('.cat-container'),
+    clicks: $('.cat-clicks'),
+    catDisplayName: $('.cat-display-name'),
+    init: () => {
+      catView.render();
+      catView.handleClicks();
+    },
+    handleClicks: () => {
+      catView.catContainer.click((event) => {
+        const TARGET = event.target.id, capsName = octopus.capitlizeCatName(TARGET);
+        if (TARGET) {
+          catView.clicks.text('');
+          // increment cat clicks
+          octopus.incrementCatClicks(TARGET);
+          catView.clicks.text(`${capsName} clicks: ${octopus.getCatClicks(TARGET)}`);
+        } else {
+          return;
+        }
+      });
+    },
+    render: () => {
+      const cat = octopus.displayCat, capsName = octopus.capitlizeCatName(cat);
+      catView.clicks.text(`${capsName} clicks: ${octopus.getCatClicks(cat)}`);
+      catView.catDisplayName.text('');
+      catView.catDisplayName.text(capsName);
+      catView.catContainer.html(`<img class="cat-display-img" id=\"${cat}\" src=\"${cat}.jpg\" alt=\"${cat} on display!\">`);
+    }
+  };
 
-function bindCounterToCat(idNumber){
-	var cat = "#cat"+idNumber
-	$(cat).click(function(){
-		var count = $(cat+" > .counter").text();
-		count = parseInt(count) + 1;
-		$(cat+" > .counter").text(count);
-	})
-}
+  let octopus = {
+    displayCat: model.catList[0],
+    capitlizeCatName: (name) => {
+      return `${name.charAt(0).toUpperCase()}${name.slice(1)}`;
+    },
+    getCatList: () => {
+      return model.catList;
+    },
+    getCatClicks: (cat) => {
+      return model.catClicksCount[cat];
+    },
+    updateCatClicks: (cat, clicks) => {
+      // update model
+      model.catClicksCount[cat] = clicks;
+    },
+    incrementCatClicks: (cat) => {
+      let clickCounter = octopus.getCatClicks(cat);
+      clickCounter++;
+      octopus.updateCatClicks(cat, clickCounter);
+    },
+    renderCat: () => {
+      catView.render();
+    },
+    init: () => {
+      model.init();
+      listView.init();
+      catView.init();
+    }
+  };
 
-for (var i=1; i<=buttons.length; i++){
-	bindButtonToCat(i);
-}
-
-for (var i=1; i<=cats.length; i++){
-	bindCounterToCat(i);
-}
-
-hideAllCats();
-$("#cat1").show();
+  octopus.init();
+});
